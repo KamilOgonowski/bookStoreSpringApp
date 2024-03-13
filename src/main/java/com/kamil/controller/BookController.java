503f79928cda61ef8b45d87d89374b6d589e9155
KamilOgonowski <kml.ogonowski@gmail.com>
2024-03-13 20:32:49 +0100

    fetching and savind the data to DB implemented

--- src/main/java/com/kamil/controller/BookController.java
+++ src/main/java/com/kamil/controller/BookController.java
@@ -1,10 +1,21 @@
 package com.kamil.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.kamil.entity.Book;
+import com.kamil.service.BookService;
 
 @Controller
 public class BookController {
+	
+	@Autowired
+	private BookService service;
+	
 	@GetMapping("/")
 	public String home() {
 		return "home";
@@ -17,8 +28,21 @@ public class BookController {
 	
 	
 	@GetMapping("available_books")
-	public String getAllBook() {
-		return "bookList";
+	public ModelAndView getAllBook() {
+		List<Book> list = service.getAllBooks();
+//		ModelAndView m = new ModelAndView();
+//		m.setViewName("bookList");
+//		m.addObject("book", list);
+		return new ModelAndView("bookList", "book",list);
+							//  ViewPage
+		
 	}
+	
+	@PostMapping("/save")
+		public String addBook(@ModelAttribute Book b) {
+			service.save(b);
+			return "redirect:/available_books";
+		}
+	
 
 }
--- src/main/java/com/kamil/service/BookService.java
+++ src/main/java/com/kamil/service/BookService.java
@@ -1,15 +1,26 @@
 package com.kamil.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kamil.entity.Book;
 import com.kamil.repository.BookRepository;
 
 @Service
 public class BookService {
 	
 	@Autowired
-	private BookRepository repo;
+	private BookRepository bRepo;
+	
+	public void save(Book b) {
+		bRepo.save(b);
+	}
+	
+	public List<Book> getAllBooks(){
+		return bRepo.findAll();
+	}
 	
 
 }
